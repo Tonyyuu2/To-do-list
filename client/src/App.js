8c2cfc1f4221ece22a231596a8d5361d2d589814
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-06-13 16:54:49 -0700

    fix styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,73 @@ const itemsFromBackend = [
   { id: uuidv4(), content: "First task" },
   { id: uuidv4(), content: "Second task" },
 ];
-
-const columnsFromBackend = [
+const columnsFromBackend = 
   {
     [uuidv4()]: {
       name: "Todo",
       items: [],
-    },
-  },
-];
+    }
+  }
 
 function App() {
-
-  const [columns, setColumns] = useState(columnsFromBackend)
+  const [columns, setColumns] = useState(columnsFromBackend);
   return (
     <div className="App">
-      <DragDropContext
-        onDropEnd={(result) => console.log(result)}
-      ></DragDropContext>
+      <DragDropContext onDropEnd={(result) => console.log(result)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable droppableId={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "lightgrey",
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500,
+                    }}
+                  >
+                    {column.items.map((item, index) => {
+                      return (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.dragHandleProps}
+                                {...provided.dragHandleProps}
+                                style={{
+                                  userSelect: "none",
+                                  padding: 16,
+                                  margin: "0 0 8px 0",
+                                  minHeight: '50px',
+                                  backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+                                  color: 'white',
+                                  ...provided.draggableProps.style
+                                }}
+                              >
+                                {item.content}
+                              </div>
+                            );
+                          }}
+                        </Draggable>
+                      );
+                    })}
+                  </div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
