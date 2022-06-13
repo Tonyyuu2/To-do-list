0315de9fe2b8925bd49191fc8e450e36e065ea16
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-06-13 15:40:36 -0700

    add state

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import "./App.css";
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { v4 as uuidv4 } from "uuid";
+
+const itemsFromBackend = [
+  { id: uuidv4(), content: "First task" },
+  { id: uuidv4(), content: "Second task" },
+];
+
+const columnsFromBackend = [
+  {
+    [uuidv4()]: {
+      name: "Todo",
+      items: [],
+    },
+  },
+];
 
 function App() {
+
+  const [columns, setColumns] = useState(columnsFromBackend)
   return (
     <div className="App">
-      <DragDropContext >
-        
-      </DragDropContext>
+      <DragDropContext
+        onDropEnd={(result) => console.log(result)}
+      ></DragDropContext>
     </div>
   );
 }
